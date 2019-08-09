68db42fb5505db9bb49b8796b6c563698e8cbddb
Nilavan Anbumozhi <nilavanrajamani@gmail.com>
2019-08-09 10:53:37 +0530

    Logging using Serilog

--- BookClub.Data/BookRepository.cs
+++ BookClub.Data/BookRepository.cs
@@ -3,20 +3,24 @@ using System.Data;
 using System.Linq;
 using BookClub.Entities;
 using Dapper;
+using Microsoft.Extensions.Logging;
 
 namespace BookClub.Data
 {
     public class BookRepository : IBookRepository
     {
         private readonly IDbConnection _db;
+        private readonly ILogger<BookRepository> logger;
 
-        public BookRepository(IDbConnection db)
+        public BookRepository(IDbConnection db, ILogger<BookRepository> logger)
         {
             _db = db;
+            this.logger = logger;
         }
 
         public List<Book> GetAllBooks()
         {
+            logger.LogInformation("GetAllBooks call");
             var books = _db.Query<Book>("GetAllBooks", commandType: CommandType.StoredProcedure)
                 .ToList();
             return books;
